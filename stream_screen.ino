87c861b44fd90db9b9d0e375608288c286820040
AndersenJ <johnathan.andersen@byu.edu>
2017-12-14 14:17:47 -0700

    Added support for different screen configurations

--- stream_screen.ino
+++ stream_screen.ino
@@ -1,14 +1,35 @@
-#define USE_SERIAL Serial
-
 #include <GxEPD.h>
-#include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w
+
+#define DEVICE_TYPE 3
+#define DEBUG 0
+
+#if DEVICE_TYPE == 0
+  #define X_RES 384
+  #define Y_RES 640
+  #define ROTATION 1
+  #include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w portrait
+#elif DEVICE_TYPE == 1
+  #define X_RES 400
+  #define Y_RES 300
+  #define ROTATION 0
+  #include <GxGDEW042T2/GxGDEW042T2.cpp>      // 4.2" b/w landscape
+#elif DEVICE_TYPE == 2
+  #define X_RES 640
+  #define Y_RES 384
+  #define ROTATION 0
+  #include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w landscape
+#elif DEVICE_TYPE == 3
+  #define X_RES 640
+  #define Y_RES 384
+  #define ROTATION 0
+  #include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w landscape
+#endif
+
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
 
 GxIO_Class io(SPI, 15, 0, 2);
 GxEPD_Class display(io, 2, 12);
-#define X_RES 384
-#define Y_RES 640
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
@@ -18,16 +39,20 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
 
-    USE_SERIAL.begin(115200);
-    // USE_SERIAL.setDebugOutput(true);
+#if DEBUG == 1
+    Serial.begin(921600);
+    // Serial.setDebugOutput(true);
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+    Serial.println();
+    Serial.println();
+    Serial.println();
+#endif
 
     for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
+    #if DEBUG == 1
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+    #endif
         delay(1000);
     }
 
@@ -41,29 +66,34 @@ void loop() {
 
         HTTPClient http;
 
-        USE_SERIAL.print("[HTTP] begin...\n");
+    #if DEBUG == 1
+        Serial.print("[HTTP] begin...\n");
+    #endif
 
         // configure server and url
-        http.begin("http://door-display.groups.et.byu.net/30aea40b54b.compressed");
-        //http.begin("192.168.1.12", 80, "/test.html");
+        http.begin("http://door-display.groups.et.byu.net/30aea40b54c.compressed");
 
-        USE_SERIAL.print("[HTTP] GET...\n");
+    #if DEBUG == 1
+        Serial.print("[HTTP] GET...\n");
+    #endif
         // start connection and send HTTP header
         int httpCode = http.GET();
         if(httpCode > 0) {
             // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+        #if DEBUG == 1
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        #endif
 
             // file found at server
             if(httpCode == HTTP_CODE_OK) {
                 display.init();
-                display.setRotation(1);
+                display.setRotation(ROTATION);
                 int cursor = 0;
                 uint8_t prevColor = 0;
                 uint8_t lastEntry;
                 int16_t counter = 0;
                 int16_t y = 0;
-                // get lenght of document (is -1 when Server sends no Content-Length header)
+                // get length of document (is -1 when Server sends no Content-Length header)
                 int len = http.getSize();
 
                 // create buffer for read
@@ -80,27 +110,34 @@ void loop() {
                     if(size) {
                         // read up to 128 byte
                         int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
-                        // write it to Serial
-                        //USE_SERIAL.write(buff, c);
                         for (int offset = 0; offset < c; offset++) {
                           if (cursor == 0 && offset == 0) {
                             lastEntry = buff[0];
+                            lastEntry = lastEntry % 2;
                             offset++;
                           }
                           counter = buff[offset];
                           if (counter == 255) {
                             for (int16_t i = cursor; i < cursor + 255; i++) {
-                              //Serial.print(i%X_RES);
-                              //Serial.print(",");
-                              //Serial.println(y+i/X_RES);
+                          #if DEBUG == 1
+                              Serial.print(lastEntry);
+                          #endif
+                              if (i % X_RES == 0)
+                            #if DEBUG == 1
+                                Serial.println("");
+                            #endif
                               display.drawPixel(i%X_RES, y+i/X_RES, lastEntry);
                             }
                             cursor += 255;
                           } else {
                             for (int16_t i = cursor; i < cursor + counter; i++) {
-                              //Serial.print(i%X_RES);
-                              //Serial.print(",");
-                              //Serial.println(y+i/X_RES);
+                          #if DEBUG == 1
+                              Serial.print(lastEntry);
+                          #endif
+                              if (i % X_RES == 0)
+                            #if DEBUG == 1
+                                Serial.println("");
+                            #endif
                               display.drawPixel(i%X_RES, y+i/X_RES, lastEntry);
                             }
                             lastEntry ^= 0x01;
@@ -111,7 +148,6 @@ void loop() {
                             y++;
                           }
                         }
-                        //cursor += c;
                         if(len > 0) {
                             len -= c;
                         }
@@ -120,12 +156,16 @@ void loop() {
                 }
                 display.update();
 
-                USE_SERIAL.println();
-                USE_SERIAL.print("[HTTP] connection closed or file end.\n");
+            #if DEBUG == 1
+                Serial.println();
+                Serial.print("[HTTP] connection closed or file end.\n");
+            #endif
 
             }
         } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        #if DEBUG == 1
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        #endif
         }
 
         http.end();
