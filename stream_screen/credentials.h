1ce66c6f3ad2ffbf48eec8ce1db3f8da612e8500
AndersenJ <johnathan.andersen@byu.edu>
2018-11-02 12:44:06 -0600

    put credentials in a separate file

---
+++ stream_screen/credentials.h
@@ -0,0 +1,6 @@
+#define ADMIN_PASSWORD "password123"  //this is the wifi password required to connect to the device when it's in admin mode
+#define DEFAULT_IMAGE_KEY "hunter2" //if you change this here, you'll also want to change it in compressImage.cpp on the server
+#define SSID0 "TestSSID0"
+#define PASSWORD0 "batman123"
+#define SSID1 "TestSSID1"
+#define PASSWORD1 "123456"
